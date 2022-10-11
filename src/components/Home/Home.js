975a44d838f588299436d5d4fefad411c916050b
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2022-10-11 19:06:11 +0600

    Some text are added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,17 @@ import './home.css'
 const Home = () => {
     return (
         <div className='home'>
-            <h1>This is Home</h1>
+            <h1>Welcome To Our Page</h1>
+
+            <div className="text-section">
             <img src={pic} alt="" />
+                <div className="text">
+                    <h2>Our Schooling and Your Victory...</h2>
+                    <hr />
+
+                    <h3>We live in the age of Science and Tecnology. Without Tecnological knowledge no one can take his proper position in this modern world. Day by day the importance of the knowledge of science and tecnology is increasing. So, Take the IT knowledge and be advanced in today's world.Best wishes for You.You should do hard work. There is no Future for the lagy people. Future is for them who are hard worker. Fiamanillah... </h3>
+                </div>
+            </div>
         </div>
     );
 };
