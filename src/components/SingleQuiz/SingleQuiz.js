c2361ed2eb0242f9d7e3ca02f2927412bf9fa470
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2022-10-12 14:55:23 +0600

    option Chacker works

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -8,29 +8,57 @@ const SingleQuiz = ({quiz}) => {
 
 
  const correctAnswer = (quiz) =>{
-console.log('correct answer is ',quiz.correctAnswer)
+// console.log('correct answer is ',quiz)
 const correct = quiz.correctAnswer;
 alert( correct)
 document.getElementById('para').innerText = correct;
  }
 
     
-    console.log('rafat',quiz)
+    // console.log('rafat',quiz)
+
+
+
+    const optionChacker = (quiz)=>{
+        console.log('optionChacker', quiz)
+        if(quiz.correctAnswer === quiz.options[0]){
+            console.log('Your answer is correct')
+        }
+        // else if(quiz.correctAnswer === quiz.options[1]){
+        //     console.log('Your Answer is correct')
+        // }
+        
+        else{
+            console.log('Your answer is wrong')
+        }
+    }
+
+    const secondChacker= (quiz) =>{
+        if(quiz.correctAnswer === quiz.options){
+            console.log('Your Answer is Right')
+        }
+        else{
+            console.log('Your answer is wrong')
+        }
+    }
+
+
+    
     return (
         <div className='border'>
             <div className="flex">
             <h1 className='style'>Question: {quiz.question} 
-            {<p id='para'></p>}
+            {/* {<p id='para'></p>} */}
             </h1>
 
             <FontAwesomeIcon onClick={()=>correctAnswer(quiz)} className='eye' icon={faEye}></FontAwesomeIcon>
             </div>
 
-            <button>
+            <button onClick={()=>optionChacker(quiz)}>
             <h2>1.{quiz.options[0]}</h2>
 
             </button>
-            <button>
+            <button onClick={()=>secondChacker(quiz)}>
                 
             <h2>2.{quiz.options[1]}</h2>
             </button>
